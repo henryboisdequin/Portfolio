d42ffb5decdca0e5a1e2e210f708fdb6da5c7f37
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-10-21 08:37:08 +0530

    finished about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -28,14 +28,26 @@ const About: React.FC<AboutProps> = ({}) => {
           <Heading size="lg" style={{ color: "#757980" }} mt={2}>
             A swimmer and programmer.
           </Heading>
-          <NextLink href="/contact">
+          <NextLink href="mailto:boisdequinhenry19@gmail.com">
             <Button leftIcon="email" variantColor="teal" variant="solid" mt={3}>
               Contact Me
             </Button>
           </NextLink>
         </Box>
-        <Box ml="auto" mt={8}>
-          <Text></Text>
+        <Box ml={100} mt={2}>
+          <Text>
+            I have been <strong>swimming</strong> competitively since the age of
+            8. I have been fortunate enough to swim all over the world: India,
+            Thailand, Nepal, Jordan, and more! My favourite events are the 200
+            free, 100 free, 200 IM, 100 fly, and the four fifties.
+          </Text>
+          <Text mt={2}>
+            I'm a self taught <strong>programmer</strong>. I've dived into the
+            fields of web development, machine learning, and mobile development.
+            I am proficient with technologies such as GraphQL, Next.js,
+            React.js, Tensorflow, Typescript, and PostgreSQL. I am currently
+            learning Flutter for mobile development.
+          </Text>
         </Box>
       </Flex>
     </Container>
