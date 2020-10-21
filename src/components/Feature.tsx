964f2e32c3735a75a5435e639d8cb5f712cabcd1
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-10-21 15:37:22 +0530

    footer done

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -11,7 +11,6 @@ import React from "react";
 
 export function Feature({ title, desc, image, icons, bg, github }) {
   const { colorMode } = useColorMode();
-  console.log(desc);
 
   return (
     <Box
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,48 @@
 import React from "react";
-import { Box, Link, Image } from "@chakra-ui/core";
+import { Link, Image, Flex, Box, useColorMode } from "@chakra-ui/core";
+
 interface FooterProps {}
 
+export const FooterItem: React.FC<{ itemArray: any }> = ({ itemArray }) => {
+  return itemArray.map((item: { href: string; src: string }) => {
+    return (
+      <Box ml={2}>
+        <Link>
+          <a target="_blank" href={item.href}>
+            <Image src={item.src} size="30px" />
+          </a>
+        </Link>
+      </Box>
+    );
+  });
+};
+
 export const Footer: React.FC<FooterProps> = ({}) => {
+  const { colorMode } = useColorMode();
+
   return (
-    <Box
-      mt={10}
-      left={0}
-      bottom={0}
-      width="100%"
-      minHeight={10}
-      textAlign="center"
-      bg="gray.500"
-    >
-      <Link>
-        <a target="_blank" href="https://github.com/henryboisdequin">
-          <Image src="/assets/github.png" size="30px" />
-        </a>
-      </Link>
+    <Box mt={10} bg={colorMode === "dark" ? "white" : undefined}>
+      <Flex
+        textAlign="center"
+        alignItems="center"
+        justifyContent="center"
+        height="120px"
+        boxSizing="border-box"
+        width="1200px"
+        maxWidth="100%"
+        borderTop={colorMode === "light" ? "2px solid #a19f9c" : "2px solid"}
+        marginTop="80px"
+        margin="auto"
+      >
+        <FooterItem
+          itemArray={[
+            {
+              href: "https://github.com/henryboisdequin",
+              src: "/assets/github.png",
+            },
+          ]}
+        />
+      </Flex>
     </Box>
   );
 };
